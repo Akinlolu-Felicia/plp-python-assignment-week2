7db0af28233293cc644bec3f12f1baf44277f2fb
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2025-08-27 15:30:51 +0100

    initial commit

---
+++ PLP-week2.py
@@ -0,0 +1,15 @@
+# creating a list name my_list
+my_list = []
+# appending the numbers
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+# inserting the last number in the second position
+my_list.insert(1, 50)
+# sorting the list
+my_list.sort()
+# finding and printing the index of 30
+index_of_30 = my_list.index(30)
+print("The list is:", my_list)
+print("The index of 30 is:", index_of_30)
